53dbc1ff648d8d0a2e05b3894f3d303c12ce196c
steelbsteel <mrtrecer@bk.ru>
2024-01-16 13:48:19 +0300

    Добавьте файлы проекта.

---
+++ Triatlon/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace Triatlon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.FPSwim = new System.Windows.Forms.TextBox();
+            this.SPSwim = new System.Windows.Forms.TextBox();
+            this.TPSwim = new System.Windows.Forms.TextBox();
+            this.FPBycicle = new System.Windows.Forms.TextBox();
+            this.sPBycicle = new System.Windows.Forms.TextBox();
+            this.TPBycicle = new System.Windows.Forms.TextBox();
+            this.FPRun = new System.Windows.Forms.TextBox();
+            this.SPRun = new System.Windows.Forms.TextBox();
+            this.TPRun = new System.Windows.Forms.TextBox();
+            this.FPTime = new System.Windows.Forms.TextBox();
+            this.SPTime = new System.Windows.Forms.TextBox();
+            this.TPTime = new System.Windows.Forms.TextBox();
+            this.WinnerTB = new System.Windows.Forms.TextBox();
+            this.BtnGetTime = new System.Windows.Forms.Button();
+            this.BtnGetWinner = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(272, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(344, 58);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Соревнование по триатлону\r\n\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(187, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Скорость";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(320, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Андрей";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(445, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Егор";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(557, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Михаил";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(161, 183);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(113, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Плавает (м/мин)";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(152, 234);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(130, 16);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Велосипед(км/час)";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(172, 282);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(81, 16);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Бег(км/час)";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(154, 330);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(129, 16);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Общее время (мин)";
+            // 
+            // FPSwim
+            // 
+            this.FPSwim.Location = new System.Drawing.Point(300, 177);
+            this.FPSwim.Name = "FPSwim";
+            this.FPSwim.Size = new System.Drawing.Size(100, 22);
+            this.FPSwim.TabIndex = 9;
+            // 
+            // SPSwim
+            // 
+            this.SPSwim.Location = new System.Drawing.Point(416, 177);
+            this.SPSwim.Name = "SPSwim";
+            this.SPSwim.Size = new System.Drawing.Size(100, 22);
+            this.SPSwim.TabIndex = 10;
+            // 
+            // TPSwim
+            // 
+            this.TPSwim.Location = new System.Drawing.Point(536, 177);
+            this.TPSwim.Name = "TPSwim";
+            this.TPSwim.Size = new System.Drawing.Size(100, 22);
+            this.TPSwim.TabIndex = 11;
+            // 
+            // FPBycicle
+            // 
+            this.FPBycicle.Location = new System.Drawing.Point(300, 228);
+            this.FPBycicle.Name = "FPBycicle";
+            this.FPBycicle.Size = new System.Drawing.Size(100, 22);
+            this.FPBycicle.TabIndex = 12;
+            // 
+            // sPBycicle
+            // 
+            this.sPBycicle.Location = new System.Drawing.Point(416, 228);
+            this.sPBycicle.Name = "sPBycicle";
+            this.sPBycicle.Size = new System.Drawing.Size(100, 22);
+            this.sPBycicle.TabIndex = 13;
+            // 
+            // TPBycicle
+            // 
+            this.TPBycicle.Location = new System.Drawing.Point(536, 228);
+            this.TPBycicle.Name = "TPBycicle";
+            this.TPBycicle.Size = new System.Drawing.Size(100, 22);
+            this.TPBycicle.TabIndex = 14;
+            // 
+            // FPRun
+            // 
+            this.FPRun.Location = new System.Drawing.Point(300, 279);
+            this.FPRun.Name = "FPRun";
+            this.FPRun.Size = new System.Drawing.Size(100, 22);
+            this.FPRun.TabIndex = 15;
+            // 
+            // SPRun
+            // 
+            this.SPRun.Location = new System.Drawing.Point(416, 279);
+            this.SPRun.Name = "SPRun";
+            this.SPRun.Size = new System.Drawing.Size(100, 22);
+            this.SPRun.TabIndex = 16;
+            // 
+            // TPRun
+            // 
+            this.TPRun.Location = new System.Drawing.Point(536, 279);
+            this.TPRun.Name = "TPRun";
+            this.TPRun.Size = new System.Drawing.Size(100, 22);
+            this.TPRun.TabIndex = 17;
+            // 
+            // FPTime
+            // 
+            this.FPTime.Enabled = false;
+            this.FPTime.Location = new System.Drawing.Point(300, 327);
+            this.FPTime.Name = "FPTime";
+            this.FPTime.Size = new System.Drawing.Size(100, 22);
+            this.FPTime.TabIndex = 18;
+            // 
+            // SPTime
+            // 
+            this.SPTime.Enabled = false;
+            this.SPTime.Location = new System.Drawing.Point(416, 327);
+            this.SPTime.Name = "SPTime";
+            this.SPTime.Size = new System.Drawing.Size(100, 22);
+            this.SPTime.TabIndex = 19;
+            // 
+            // TPTime
+            // 
+            this.TPTime.Enabled = false;
+            this.TPTime.Location = new System.Drawing.Point(536, 327);
+            this.TPTime.Name = "TPTime";
+            this.TPTime.Size = new System.Drawing.Size(100, 22);
+            this.TPTime.TabIndex = 20;
+            // 
+            // WinnerTB
+            // 
+            this.WinnerTB.Enabled = false;
+            this.WinnerTB.Location = new System.Drawing.Point(536, 393);
+            this.WinnerTB.Multiline = true;
+            this.WinnerTB.Name = "WinnerTB";
+            this.WinnerTB.Size = new System.Drawing.Size(228, 44);
+            this.WinnerTB.TabIndex = 21;
+            // 
+            // BtnGetTime
+            // 
+            this.BtnGetTime.Location = new System.Drawing.Point(175, 374);
+            this.BtnGetTime.Name = "BtnGetTime";
+            this.BtnGetTime.Size = new System.Drawing.Size(106, 43);
+            this.BtnGetTime.TabIndex = 22;
+            this.BtnGetTime.Text = "Вычислить время";
+            this.BtnGetTime.UseVisualStyleBackColor = true;
+            this.BtnGetTime.Click += new System.EventHandler(this.BtnGetTime_Click);
+            // 
+            // BtnGetWinner
+            // 
+            this.BtnGetWinner.Location = new System.Drawing.Point(398, 394);
+            this.BtnGetWinner.Name = "BtnGetWinner";
+            this.BtnGetWinner.Size = new System.Drawing.Size(106, 43);
+            this.BtnGetWinner.TabIndex = 23;
+            this.BtnGetWinner.Text = "Вычислить чемпиона";
+            this.BtnGetWinner.UseVisualStyleBackColor = true;
+            this.BtnGetWinner.Click += new System.EventHandler(this.BtnGetWinner_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnGetWinner);
+            this.Controls.Add(this.BtnGetTime);
+            this.Controls.Add(this.WinnerTB);
+            this.Controls.Add(this.TPTime);
+            this.Controls.Add(this.SPTime);
+            this.Controls.Add(this.FPTime);
+            this.Controls.Add(this.TPRun);
+            this.Controls.Add(this.SPRun);
+            this.Controls.Add(this.FPRun);
+            this.Controls.Add(this.TPBycicle);
+            this.Controls.Add(this.sPBycicle);
+            this.Controls.Add(this.FPBycicle);
+            this.Controls.Add(this.TPSwim);
+            this.Controls.Add(this.SPSwim);
+            this.Controls.Add(this.FPSwim);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox FPSwim;
+        private System.Windows.Forms.TextBox SPSwim;
+        private System.Windows.Forms.TextBox TPSwim;
+        private System.Windows.Forms.TextBox FPBycicle;
+        private System.Windows.Forms.TextBox sPBycicle;
+        private System.Windows.Forms.TextBox TPBycicle;
+        private System.Windows.Forms.TextBox FPRun;
+        private System.Windows.Forms.TextBox SPRun;
+        private System.Windows.Forms.TextBox TPRun;
+        private System.Windows.Forms.TextBox FPTime;
+        private System.Windows.Forms.TextBox SPTime;
+        private System.Windows.Forms.TextBox TPTime;
+        private System.Windows.Forms.TextBox WinnerTB;
+        private System.Windows.Forms.Button BtnGetTime;
+        private System.Windows.Forms.Button BtnGetWinner;
+    }
+}
+
---
+++ Triatlon/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Triatlon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string GetTime(double swim, double bycicle, double run)
+        {
+            if (swim <= 0 || String.IsNullOrEmpty(Convert.ToString(swim)) ||
+                bycicle <= 0 || String.IsNullOrEmpty(Convert.ToString(bycicle)) ||
+                run <= 0 || String.IsNullOrEmpty(Convert.ToString(run)))
+            {
+                return ("Ошибка");
+            }
+
+            double time = 1500 / swim + 40 / bycicle * 60 + 10 / run * 60;
+            return Convert.ToString(time);
+        }
+        private void BtnGetTime_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FPTime.Text = GetTime(double.Parse(FPSwim.Text), double.Parse(FPBycicle.Text), double.Parse(FPRun.Text));
+                SPTime.Text = GetTime(double.Parse(SPSwim.Text), double.Parse(sPBycicle.Text), double.Parse(SPRun.Text));
+                TPTime.Text = GetTime(double.Parse(TPSwim.Text), double.Parse(TPBycicle.Text), double.Parse(TPRun.Text));
+            }
+            catch
+            {
+                FPTime.Text = "Ошибка";
+                SPTime.Text = "заполнения";
+                TPTime.Text = "данных";
+            }
+        }
+
+        private void BtnGetWinner_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(FPTime.Text) ||
+               String.IsNullOrEmpty(SPTime.Text) ||
+               String.IsNullOrEmpty(TPTime.Text))
+            {
+                WinnerTB.Text = "Сначала вычислите время";
+                return;
+            }
+
+            if (FPTime.Text == "Ошибка" ||
+                FPTime.Text == "Ошибка" ||
+                FPTime.Text == "Ошибка")
+            {
+                WinnerTB.Text = "Ошибка. Проверьте корректность данных в полях времени.";
+                return;
+            }
+
+            string max_name;
+
+            MaxAndName(double.Parse(FPTime.Text), double.Parse(SPTime.Text), double.Parse(TPTime.Text),
+            "Андрей", "Егор", "Михаил",
+            out max_name);
+
+            WinnerTB.Text = max_name;
+
+        }
+
+        void MaxAndName(double x1, double x2, double x3,
+            string name1, string name2, string name3,
+            out string name_max)
+        {
+            double min = double.MaxValue; //вводим переменную для максимума с начальным значением MinValue
+            name_max = "";
+            if (x1 < min)
+            {
+                min = x1; name_max = name1; //максимум из одного значения и имя 
+            }
+            if (x2 < min)
+            {
+                min = x2; name_max = name2; //максимум из двух значений и имя 
+            }
+            if (x3 < min)
+            {
+                name_max = name3; //максимум из трех значений и имя 
+            }
+        }
+
+    }
+}
